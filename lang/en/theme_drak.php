59016ab46ed98c3fd97004c4b59ae88f3619ac58
Kvetuse Husakova <huskvenimrah@gmail.com>
2021-06-21 10:45:05 +0200

    Testing...

---
+++ lang/en/theme_drak.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package   theme_drak
+ * @copyright 2021 Květa
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['choosereadme'] = "This theme is a child theme of drak";
+$string['pluginname'] = "drak";
+$string['region-side-pre'] = "Right";
+$string['brandcolor'] = 'Brand colour';  
+$string['brandcolor_desc'] = 'The accent colour.';    
+$string['configtitle'] = 'Drak settings';  
+$string['generalsettings'] = 'General settings';
+$string['presetfiles'] = 'Additional theme preset files';
+$string['preset'] = 'Theme preset'; 
+$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';  
+$string['rawscss'] = 'Raw SCSS';   
+$string['rawscsspre'] = 'Raw initial SCSS';  
+// Background image for login page.                                                                                                 
+$string['loginbackgroundimage'] = 'Login page background image';                                                                    
+// Background image for login page.                                                                                                 
+$string['loginbackgroundimage_desc'] = 'An image that will be stretched to fill the background of the login page.';
